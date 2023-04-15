a1353398a920ceeca74af4820618ec6b40f303fd
umarak <screaminglhf@gmail.com>
2023-04-15 20:51:18 +0300

    Запилен дизайн, добавлены записи в таблицу. Осталось только отфильтровать!

--- farmalook/filter/admin.py
+++ farmalook/filter/admin.py
@@ -4,7 +4,7 @@ from .models import Table
 
 class FilterAdmin(admin.ModelAdmin):
     list_display = ('pk', 'company_name', 'reg_date', 'country_company', 'aid_form', 'aid_name', 'mnn', 'dru')
-    list_editable = ('company_name','country_company', 'aid_form', 'aid_name', 'mnn', 'dru',)
+    list_editable = ('company_name','country_company', 'aid_form', 'aid_name', 'mnn', 'dru', 'reg_date',)
     search_fields = ('company_name',)
     list_filter = ('reg_date',)
     empty_value_display = '-пусто-'
