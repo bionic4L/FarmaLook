10d3ec40196c0802d4f6833e56e240da4c1dfea0
Kivrol <95294731+Kivrol@users.noreply.github.com>
2023-04-15 17:56:35 +0300

    test

--- farmalook/farmalook/urls.py
+++ farmalook/farmalook/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# Я е?№*"Ь с питоном, мне норм
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('filter.urls')),
